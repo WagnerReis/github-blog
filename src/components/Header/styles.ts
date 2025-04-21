e68027ea02e4d75f4aa595c549e13a6251d98a6b
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-21 19:40:41 -0300

    feat: add Header component and cover image

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 18.5rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+  }
+`;
