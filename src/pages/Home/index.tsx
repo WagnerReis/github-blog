36f9e7289fd34a9d3229ade65b5280ad3b505551
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-23 14:16:18 -0300

    feat(Home): add search functionality for issues

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,29 @@ import { useIssues } from "../../hooks/useIssues";
 import { Card } from "./components/Card";
 import { Profile } from "./components/Profile";
 import { HomeContainer, MainHeader, CardGrid } from "./styles";
+import { debounce } from "lodash";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 
 export function Home() {
-  const { issues } = useIssues();
+  const { issues, fetchIssues } = useIssues();
+  const [search, setSearch] = useState("");
+
+  const handleSearchChange = useCallback((value: string) => {
+    const debouncedSearch = debounce((searchValue: string) => {
+      setSearch(searchValue);
+    }, 2000);
+    debouncedSearch(value);
+  }, []);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    handleSearchChange(event.target.value);
+  };
+
+  useEffect(() => {
+    fetchIssues(search);
+  }, [fetchIssues, search]);
+
+  console.log("search: ", search);
 
   return (
     <HomeContainer>
@@ -18,7 +38,11 @@ export function Home() {
         <span>{issues.totalCount} publicações</span>
       </MainHeader>
 
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input
+        type="text"
+        placeholder="Buscar conteúdo"
+        onChange={handleInputChange}
+      />
 
       <CardGrid>
         {issues.items.map((issue) => {
